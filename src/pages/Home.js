70a9023f64f46f6953d8d2d5d085f69814a99544
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-03-31 01:35:57 +0500

    Home.js light theme completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,35 +2,35 @@ import './css/Home.css'
 
 export default function () {
 
-    document.addEventListener('DOMContentLoaded', () => {
-        if ('dark' === 'dark') {
-            let elements = document.querySelectorAll('.mantine-1w6i6a8');
-            for(let i = 0; i < elements.length; i++){
-                const element  = elements[i];
-                element.style.backgroundColor = 'rgba(37,38,43,255)';
-            }
-            elements = document.querySelectorAll('.mantine-9tuz4g');
-            for (let i = 0; i < elements.length; i++) {
-                const element = elements[i];
-                element.style.backgroundColor = 'rgba(37,38,43,255)'
-            }
-            elements = document.querySelectorAll('.mantine-s9arro');
-            for (let i = 0; i < elements.length; i++) {
-                const element = elements[i];
-                element.style.color = 'rgba(191,192,197,255)';
-            }
-            elements = document.querySelectorAll('.mantine-6qpjlo');
-            for (let i = 0; i < elements.length; i++) {
-                const element = elements[i];
-                element.style.color = 'rgba(191,192,197,255)'
-            }
-            elements = document.querySelectorAll('.mantine-qxe4dyy');
-            for (let i = 0; i < elements.length; i++) {
-                const element = elements[i];
-                element.style.color = 'rgba(191,192,197,255)'
-            }
-        }
-    });
+    // document.addEventListener('DOMContentLoaded', () => {
+    //     if ('dark' === 'dark') {
+    //         let elements = document.querySelectorAll('.mantine-1w6i6a8');
+    //         for(let i = 0; i < elements.length; i++){
+    //             const element  = elements[i];
+    //             element.style.backgroundColor = 'rgba(37,38,43,255)';
+    //         }
+    //         elements = document.querySelectorAll('.mantine-9tuz4g');
+    //         for (let i = 0; i < elements.length; i++) {
+    //             const element = elements[i];
+    //             element.style.backgroundColor = 'rgba(37,38,43,255)'
+    //         }
+    //         elements = document.querySelectorAll('.mantine-s9arro');
+    //         for (let i = 0; i < elements.length; i++) {
+    //             const element = elements[i];
+    //             element.style.color = 'rgba(191,192,197,255)';
+    //         }
+    //         elements = document.querySelectorAll('.mantine-6qpjlo');
+    //         for (let i = 0; i < elements.length; i++) {
+    //             const element = elements[i];
+    //             element.style.color = 'rgba(191,192,197,255)'
+    //         }
+    //         elements = document.querySelectorAll('.mantine-qxe4dyy');
+    //         for (let i = 0; i < elements.length; i++) {
+    //             const element = elements[i];
+    //             element.style.color = 'rgba(191,192,197,255)'
+    //         }
+    //     }
+    // });
 
     const user_id = '1122332';
     let cart = {
@@ -61,6 +61,14 @@ export default function () {
             id: '5',
             name: 'Dessert',
         },
+        {
+            id : '6',
+            name : 'Bar'
+        },
+        {
+            id : '7',
+            name : 'Other'
+        }
 
     ]
     
