8152c916c77768fb8415ecc9d07e43103398e078
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-03-31 00:47:05 +0500

    theme problem test

--- src/App.js
+++ src/App.js
@@ -2,9 +2,46 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import ShowProduct from './pages/ShowProduct';
-import Cart from './pages/Cart';
+import Cart from './pages/Cart'
+import { useState } from 'react';
+
+const tg = window.Telegram.WebApp;
 
 function App() {
+
+  useState(()=>{
+    tg.ready();
+  })
+  document.addEventListener('DOMContentLoaded', () => {
+    let theme = tg.colorScheme;
+    if (theme === 'dark') {
+        let elements = document.querySelectorAll('.mantine-1w6i6a8');
+        for(let i = 0; i < elements.length; i++){
+            const element  = elements[i];
+            element.style.backgroundColor = 'rgba(37,38,43,255)';
+        }
+        elements = document.querySelectorAll('.mantine-9tuz4g');
+        for (let i = 0; i < elements.length; i++) {
+            const element = elements[i];
+            element.style.backgroundColor = 'rgba(37,38,43,255)'
+        }
+        elements = document.querySelectorAll('.mantine-s9arro');
+        for (let i = 0; i < elements.length; i++) {
+            const element = elements[i];
+            element.style.color = 'rgba(191,192,197,255)';
+        }
+        elements = document.querySelectorAll('.mantine-6qpjlo');
+        for (let i = 0; i < elements.length; i++) {
+            const element = elements[i];
+            element.style.color = 'rgba(191,192,197,255)'
+        }
+        elements = document.querySelectorAll('.mantine-qxe4dyy');
+        for (let i = 0; i < elements.length; i++) {
+            const element = elements[i];
+            element.style.color = 'rgba(191,192,197,255)'
+        }
+    }
+});
   return (
     <div>
       <Router>
