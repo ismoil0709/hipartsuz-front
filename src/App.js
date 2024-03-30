664d6f0fb39198fa3bfa409ec7198ec9fdb84b1f
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-03-31 01:02:57 +0500

    theme problem test

--- src/App.js
+++ src/App.js
@@ -18,28 +18,30 @@ function App() {
         let elements = document.querySelectorAll('.mantine-1w6i6a8');
         for(let i = 0; i < elements.length; i++){
             const element  = elements[i];
-            element.style.backgroundColor = 'rgba(37,38,43,255)';
+            element.innerHTML = ('SALOM')
+            // alert(element)
+            // element.style.backgroundColor = 'rgba(37,38,43,255)';
         }
-        elements = document.querySelectorAll('.mantine-9tuz4g');
-        for (let i = 0; i < elements.length; i++) {
-            const element = elements[i];
-            element.style.backgroundColor = 'rgba(37,38,43,255)'
-        }
-        elements = document.querySelectorAll('.mantine-s9arro');
-        for (let i = 0; i < elements.length; i++) {
-            const element = elements[i];
-            element.style.color = 'rgba(191,192,197,255)';
-        }
-        elements = document.querySelectorAll('.mantine-6qpjlo');
-        for (let i = 0; i < elements.length; i++) {
-            const element = elements[i];
-            element.style.color = 'rgba(191,192,197,255)'
-        }
-        elements = document.querySelectorAll('.mantine-qxe4dyy');
-        for (let i = 0; i < elements.length; i++) {
-            const element = elements[i];
-            element.style.color = 'rgba(191,192,197,255)'
-        }
+        // elements = document.querySelectorAll('.mantine-9tuz4g');
+        // for (let i = 0; i < elements.length; i++) {
+        //     const element = elements[i];
+        //     element.style.backgroundColor = 'rgba(37,38,43,255)'
+        // }
+        // elements = document.querySelectorAll('.mantine-s9arro');
+        // for (let i = 0; i < elements.length; i++) {
+        //     const element = elements[i];
+        //     element.style.color = 'rgba(191,192,197,255)';
+        // }
+        // elements = document.querySelectorAll('.mantine-6qpjlo');
+        // for (let i = 0; i < elements.length; i++) {
+        //     const element = elements[i];
+        //     element.style.color = 'rgba(191,192,197,255)'
+        // }
+        // elements = document.querySelectorAll('.mantine-qxe4dyy');
+        // for (let i = 0; i < elements.length; i++) {
+        //     const element = elements[i];
+        //     element.style.color = 'rgba(191,192,197,255)'
+        // }
     }
 });
   return (
