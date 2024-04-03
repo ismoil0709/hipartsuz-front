443d42b0b3cb304e2149550051607168c9dfdd7e
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-04-04 00:07:30 +0500

    dark theme completed

--- src/util/setTheme.js
+++ src/util/setTheme.js
@@ -3,6 +3,7 @@ const tg = window.Telegram.WebApp;
 
 export default function setTheme(className){
     if('dark' === 'dark'){
+        document.querySelector('body').style.backgroundColor = '#1a1b1e';
         return className + '-dark';
     }else{
         return className + '-light';
